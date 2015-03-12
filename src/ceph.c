7e9f502f2b379586cd2f5a70399a4bf402febbca
root <root@n133.(none)>
2015-03-11 23:48:26 -0700

    Add server files

--- src/ceph.c
+++ src/ceph.c
@@ -4,7 +4,6 @@
         osd pgp bits = 5  ; (invalid, but ceph should cope!)
         osd crush chooseleaf type = 0
         osd pool default min size = 1
-        osd pool default hint size = 6
         osd failsafe full ratio = .99
         mon osd full ratio = .99
         mon data avail warn = 10
@@ -13,39 +12,41 @@
         osd pool default erasure code profile = plugin=jerasure technique=reed_sol_van k=2 m=1 ruleset-failure-domain=osd
         rgw frontends = fastcgi, civetweb port=8000
         filestore fd cache size = 32
-        run dir = /home/fedora/tmpceph/out
+        run dir = /root/script-ceph/out
         auth supported = cephx
+	public network = 10.1.0.0/24
+	cluster network = 10.6.0.0/24
 
 [client]
-        keyring = /home/fedora/tmpceph/ceph.client.admin.keyring
-        log file = /home/fedora/tmpceph/out/$name.$pid.log
-        admin socket = /home/fedora/tmpceph/out/$name.$pid.asok
+        keyring = /root/script-ceph/ceph.client.admin.keyring
+        log file = /root/script-ceph/out/$name.$pid.log
+        admin socket = /root/script-ceph/out/$name.$pid.asok
 
 [mds]
 
-        log file = /home/fedora/tmpceph/out/$name.$pid.log
-        admin socket = /home/fedora/tmpceph/out/$name.$pid.asok
+        log file = /root/script-ceph/out/$name.$pid.log
+        admin socket = /root/script-ceph/out/$name.$pid.asok
 	chdir = ""
-	pid file = /home/fedora/tmpceph/out/$name.pid
-        heartbeat file = /home/fedora/tmpceph/out/$name.heartbeat
+	pid file = /root/script-ceph/out/$name.pid
+        heartbeat file = /root/script-ceph/out/$name.heartbeat
 
 
         debug ms = 1
         mds debug frag = true
         mds debug auth pins = true
         mds debug subtrees = true
-        mds data = /home/fedora/tmpceph/mds/mds.$id
+        mds data = /root/script-ceph/mds/mds.$id
 
 [osd]
 
-        log file = /home/fedora/tmpceph/out/$name.$pid.log
-        admin socket = /home/fedora/tmpceph/out/$name.$pid.asok
+        log file = /root/ceph/src/out/$name.$pid.log
+        admin socket = /root/script-ceph/out/$name.$pid.asok
 	chdir = ""
-	pid file = /home/fedora/tmpceph/out/$name.pid
-        heartbeat file = /home/fedora/tmpceph/out/$name.heartbeat
+	pid file = /root/script-ceph/out/$name.pid
+        heartbeat file = /root/script-ceph/out/$name.heartbeat
 
-        osd data = /home/fedora/tmpceph/osd/osd$id
-        osd journal = /home/fedora/tmpceph/osd/osd$id.journal
+        osd data = /root/script-ceph/osd/osd$id
+        osd journal = /root/script-ceph/osd/osd$id/osd$id.journal
         osd journal size = 100
         osd class tmp = out
         osd class dir = .libs
@@ -66,32 +67,83 @@
         mon osd allow primary affinity = true
         mon reweight min pgs per osd = 4
 
-        log file = /home/fedora/tmpceph/out/$name.$pid.log
-        admin socket = /home/fedora/tmpceph/out/$name.$pid.asok
+        log file = /root/script-ceph/out/$name.$pid.log
+        admin socket = /root/script-ceph/out/$name.$pid.asok
 	chdir = ""
-	pid file = /home/fedora/tmpceph/out/$name.pid
-        heartbeat file = /home/fedora/tmpceph/out/$name.heartbeat
+	pid file = /root/script-ceph/out/$name.pid
+        heartbeat file = /root/script-ceph/out/$name.heartbeat
 
 
 	debug mon = 10
         debug ms = 1
 
-        mon cluster log file = /home/fedora/tmpceph/out/cluster.mon.$id.log
+        mon cluster log file = /root/script-ceph/out/cluster.mon.$id.log
 [global]
 
 [mon.a]
-        host = ceps-build
-        mon data = /home/fedora/tmpceph/mon/mon.a
-        mon addr = 127.0.0.1:6789
+        host = n133
+        mon data = /root/script-ceph/mon/mon.a
+        mon addr = 10.1.0.133:6789
 [osd.0]
-        host = ceps-build
+	public addr = 10.1.0.133
+	cluster addr = 10.6.0.133
 [osd.1]
-        host = ceps-build
+	public addr = 10.1.0.133
+	cluster addr = 10.6.0.133
 [osd.2]
-        host = ceps-build
-[mds.a]
-        host = ceps-build
-[mds.b]
-        host = ceps-build
-[mds.c]
-        host = ceps-build
+	public addr = 10.1.0.133
+	cluster addr = 10.6.0.133
+[osd.3]
+	public addr = 10.1.0.133
+	cluster addr = 10.6.0.133
+[osd.4]
+	public addr = 10.1.0.133
+	cluster addr = 10.6.0.133
+[osd.5]
+	public addr = 10.1.0.133
+	cluster addr = 10.6.0.133
+[osd.6]
+	public addr = 10.1.0.133
+	cluster addr = 10.6.0.133
+[osd.7]
+	public addr = 10.1.0.134
+	cluster addr = 10.6.0.134
+[osd.8]
+	public addr = 10.1.0.134
+	cluster addr = 10.6.0.134
+[osd.9]
+	public addr = 10.1.0.134
+	cluster addr = 10.6.0.134
+[osd.10]
+	public addr = 10.1.0.134
+	cluster addr = 10.6.0.134
+[osd.11]
+	public addr = 10.1.0.134
+	cluster addr = 10.6.0.134
+[osd.12]
+	public addr = 10.1.0.134
+	cluster addr = 10.6.0.134
+[osd.13]
+	public addr = 10.1.0.134
+	cluster addr = 10.6.0.134
+[osd.14]
+	public addr = 10.1.0.135
+	cluster addr = 10.6.0.135
+[osd.15]
+	public addr = 10.1.0.135
+	cluster addr = 10.6.0.135
+[osd.16]
+	public addr = 10.1.0.135
+	cluster addr = 10.6.0.135
+[osd.17]
+	public addr = 10.1.0.135
+	cluster addr = 10.6.0.135
+[osd.18]
+	public addr = 10.1.0.135
+	cluster addr = 10.6.0.135
+[osd.19]
+	public addr = 10.1.0.135
+	cluster addr = 10.6.0.135
+[osd.20]
+	public addr = 10.1.0.135
+	cluster addr = 10.6.0.135
--- src/rbd_test.py
+++ src/rbd_test.py
@@ -1,18 +1,22 @@
 import rados
 import rbd_hint
+import sys
+name=sys.argv[1]
 with rados.Rados(conffile='ceph.conf') as cluster:
     with cluster.open_ioctx('rbd') as ioctx:
 	print "Before Init"
         rbd_inst = rbd_hint.RBD()
-        size = 4 * 1024**3  # 4 GiB
-	print "Before Remove"
-        rbd_inst.create(ioctx, 'test', size)
-	print "After Create"
-        with rbd_hint.Image(ioctx, 'test') as image:
-		print "Before Write"
-        	data = 'foo' * 200
-        	image.write_with_hint(data, 0, "osd.2")
-		print "After Write"
-		str = image.read_with_hint(0, len(data), "osd.2")
-		print str
+	rbd_inst.create(ioctx, name, 4*1024**3)
+	print "Created"
+        with rbd_hint.Image(ioctx, name) as image:
+		print "Opened"
+		strlen = 0
+		for i in range(0, 1):
+        		data = 'f' * 64;
+			image.write_with_hint(data, strlen,"osd.1")
+			print "Writen"
+			strlen += len(data)
+			print image.read_with_hint(0, strlen, "osd.1")
+			print "Read"
 		image.close()
+	rbd_inst.remove(ioctx, name)
